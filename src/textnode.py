c373f999521876842fd46f6576719fa9a3e5ed1d
The_Legolas <lucalegom@gmail.com>
2025-10-05 23:45:32 +0200

    #

--- src/textnode.py
+++ src/textnode.py
@@ -27,4 +27,6 @@ class TextNode():
                 and self.url == other.url)
     
     def __repr__(self):
-        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+        if self.url is not None:
+            return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+        return f"TextNode({self.text}, {self.text_type.value})"
