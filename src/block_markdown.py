d533f309c82e852c35e5f18560c8d78e31f6c750
The_Legolas <lucalegom@gmail.com>
2025-10-19 22:18:42 +0200

    finally finished the markdown to html node function onto the next topic (this only took so many hours eheh (why am I doing this to myself))

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -9,10 +9,10 @@ class BlockType(Enum):
     HEADING = "heading"
     CODE = "code"
     QUOTE = "quote"
-    UNORDERED_LIST = "unordered_list"
-    ORDERED_LIST = "ordered_list"
+    UNORDERED_LIST = "ul"
+    ORDERED_LIST = "ol"
 
-def markdown_to_blocks(markdown):
+def markdown_to_blocks(markdown: str) -> list[str]:
     parts = re.split(r"\n{2,}", markdown.strip())
     clean = [p.strip() for p in parts if p.strip()]
     return clean
@@ -41,20 +41,19 @@ def markdown_to_html_node(markdown: str) -> HTMLNode:
         block_type = block_to_block_type(block)
 
         if block_type == BlockType.PARAGRAPH:
-            child = ParentNode("p", text_to_children(block.strip()))
+            child = ParentNode("p", text_to_children(block.replace("\n", " ").strip()))
         elif block_type == BlockType.HEADING:
             child = make_heading(block)
         elif block_type == BlockType.CODE:
-            child = ParentNode("pre", make_code(block))
+            child = ParentNode("pre", [make_code(block)])
         elif block_type == BlockType.QUOTE:
-            child = ParentNode("blockquote", text_to_children(block))
+            child = ParentNode("blockquote", make_quote(block))
         elif block_type == BlockType.UNORDERED_LIST:
-            child = LeafNode("ul", block)
+            child = ParentNode("ul", make_unordered_list(block))
         elif block_type == BlockType.ORDERED_LIST:
-            child = LeafNode("ol", block)
+            child = ParentNode("ol", make_ordered_list(block))
         else:
-            print("Unknown block type:", block_type)
-            continue
+            raise TypeError("Unknown block type:", block_type)
         parent.children.append(child)
     return parent
     
@@ -66,11 +65,11 @@ def text_to_children(text: str) -> list[HTMLNode]:
     return html_nodes
 
 def make_heading(text: str) -> HTMLNode:
-    m = re.match(r'^(\#+\s{1})(.*)', text)
-    level = len(m.group(1).strip())
+    match = re.match(r'^(\#+\s{1})(.*)', text.strip())
+    level = len(match.group(1).rstrip())
     if level > 6:
         raise ValueError("Too many leading '#' (max 6)")
-    node = text_to_children(m.group(2).strip())
+    node = text_to_children(match.group(2).strip())
     return ParentNode(f"h{level}", node)
 
 def make_code(text: str) -> HTMLNode:
@@ -89,22 +88,47 @@ def make_code(text: str) -> HTMLNode:
         
         inline = first[first.find("```")+3:]
         node = "".join([inline] + lines[1:-1])
-    code_child = LeafNode("code", node)
-    return ParentNode("pre", [code_child])
-
-
-def make_qoute(text):
-    ...
+    return LeafNode("code", node)
     
 
-def make_blockqoute(text):
-    ...
+def make_quote(text: str) -> list[HTMLNode]:
+    lines = text.splitlines(keepends=True)
+    stripped_lines = []
+    for line in lines:
+        if not line.startswith(">"):
+            raise ValueError(f"Not a true markdown quote. It should start with '>'. {line!r}")
+        stripped_lines.append(line.lstrip("> ").strip())
+    joined_text = " ".join(stripped_lines)
+    return text_to_children(joined_text)
 
-def make_unordered_list(text):
-    ...
+def make_unordered_list(text: str) -> HTMLNode:
+    lines = text.splitlines()
+    ul_node = []
+    for line in lines:
+        line = line.strip()
+        if line.startswith("*"):
+            line = line.lstrip("* ")
+        elif line.startswith("-"):
+            line = line.lstrip("- ")
+        else:
+            raise ValueError("This list does not contain '*' or '-' at the start")
+        ul_node.append(ParentNode("li", text_to_children(line)))
+    return ul_node
 
-def make_ordered_list(text):
-    ...
+def make_ordered_list(text: str) -> HTMLNode:
+    lines = text.splitlines()
+    ol_node = []
+    for i, line in enumerate(lines, start=1):
+        line = line.strip()
+        if not line.startswith(f"{i}."):
+            raise ValueError("This is not an ordered list as it does not start with a digit and a '.'")
+        line = line[len(f"{i}."):].strip()
+        ol_node.append(ParentNode("li", text_to_children(line)))
+    return ol_node
 
-print(make_qoute("```this is code,\n but\nthis is also code\n```"))
+
+
+
+#print(make_quote("> Hello\n> World"))
+#print(make_heading("### awj"))
 #print(make_qoute("```this is code, butthis is also code\n```"))
