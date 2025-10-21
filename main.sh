0cfd40a3e9869a216e005f6fd9663b225b0accab
The_Legolas <lucalegom@gmail.com>
2025-10-21 17:29:33 +0200

    finished the generation part only need to actually make the page

--- main.sh
+++ main.sh
@@ -1,1 +1,1 @@
-python3 src/main.py
+python3 src/main.py src_path, dst_path
--- src/main.py
+++ src/main.py
@@ -1,7 +1,47 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+import sys
 
-def main():
-    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-    print(node)
+if len(sys.argv) != 3:
+        print("Usage: python3 src/main.py <src_path> <dst_path>")
+        sys.exit(1)
+src_path = sys.argv[1]  # e.g., "static/"
+dst_path = sys.argv[2]  # e.g., "public/"
 
-main()
+
+def main(src_path, dst_path):
+    print(f"Source: {src_path}")
+    print(f"Destination: {dst_path}")
+
+    if os.path.exists(src_path):
+        try:
+            shutil.rmtree(dst_path)    #Only re-activate when project is ready
+        except:
+            os.makedirs("public")
+
+        all_paths = process_directory(src_path, src_path)
+        
+        for relative_path in all_paths:
+            print("Found file:", relative_path)
+            src_full_path = os.path.join(src_path, relative_path) 
+            # Build full destination path
+            dst_full_path = os.path.join(dst_path, relative_path)  
+            # Get the destination directory (parent of the file)
+            dst_dir = os.path.dirname(dst_full_path)
+            os.makedirs(dst_dir, exist_ok=True)
+            shutil.copy(src_full_path, dst_full_path)
+
+def process_directory(src_path, root_src_path):
+    path = []
+    if os.path.exists(src_path):
+        for entry in os.listdir(src_path):
+            full_path = os.path.join(src_path, entry)
+            relative_path = os.path.relpath(full_path, start=root_src_path)
+            if os.path.isdir(full_path):
+                path += process_directory(full_path, root_src_path)
+            else:
+                path.append(relative_path)
+    return path
+
+
+main(src_path, dst_path)
